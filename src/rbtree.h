bc7ccda0f2184c6b79ddd29abd2c0fdd3e6dabed
orian <pawelszczur@gmail.com>
2014-04-26 21:57:03 +0200

    Add extra tests and fix all delete problems.

--- src/rbtree.h
+++ src/rbtree.h
@@ -1,4 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 namespace trilib {
+
+namespace {
 
 template <typename ValueT>
 class RBTreeNode {
@@ -36,6 +43,7 @@ class RBTreeNode {
 
   void SetColorRed() { properties &= ~1; }
   void SetColorBlack() { properties |= 1; }
+  void SetColor(bool is_black) { is_black ? SetColorBlack() : SetColorRed(); }
   bool IsColorRed() const { return (properties & 1) == 0; }
   bool IsColorBlack() const { return properties & 1; }
 
@@ -64,52 +72,94 @@ const unsigned int RBTreeNode<ValueT>::kLeftChild = 4;
 template <typename ValueT>
 const unsigned int RBTreeNode<ValueT>::kRightChild = 8;
 
-namespace {
-
 // helper methods
 template <typename ValueT>
 const ValueT& GetValue(const RBTreeNode<ValueT>* const ptr) {
-  return ptr->value;
+  return ptr->value_;
 }
 
 template <typename ValueT>
 const ValueT& GetValue(const RBTreeNode<ValueT>& ref) {
-  return ref.value;
+  return ref.value_;
 }
 
 template <typename ValueT, typename CompT>
-bool RunCmp(const RBTreeNode<ValueT>* const a,
-            const RBTreeNode<ValueT>* const b, const CompT& comp) {
-  return comp(a->value_, b->value_);
+bool RunCmp(const ValueT* const a, const ValueT* const b, const CompT& comp) {
+  return comp(GetValue(a), GetValue(b));
 }
 
-// template<typename NodeT, typename CompT>
-// bool RunCmp(const NodeT a, const NodeT b) {
-//   static CompT comp;
-//   return comp(GetValue(a), GetValue(b));
-// }
-
-// template<typename NodeT, typename CompT>
-// bool RunCmp(const NodeT a, const NodeT b, const CompT& comp) {
-//   return comp(GetValue(a), GetValue(b));
-// }
-
-// template<typename NodeT, typename CompT>
-// bool RunCmp(const NodeT a, const NodeT b, const CompT& comp) {
-//   return comp(GetValue(a), GetValue(b));
-// }
-
-}  // namespace
+template <typename ValueT, typename CompT>
+bool RunCmp(const ValueT& a, const ValueT& b, const CompT& comp) {
+  return comp(GetValue(a), GetValue(b));
+}
 
 template <typename ValueT, typename CompT>
-bool CheckIsBinarySearchTree(const RBTreeNode<ValueT>* const root) {
-  return (!root->HasLeftChild() ||
-          (RunCmp<ValueT, CompT>(root->left_child, root, CompT()) &&
-           CheckIsBinarySearchTree<ValueT, CompT>(
-               root->left_child))) &&  // left child
+bool CheckIsBinarySearchTree(const ValueT& root) {
+  return (!root.HasLeftChild() || (RunCmp(*root.left_child, root, CompT()) &&
+                                   CheckIsBinarySearchTree<ValueT, CompT>(
+                                       *root.left_child))) &&  // left child
+         (!root.HasRightChild() ||
+          (RunCmp(root, *root.right_child, CompT()) &&
+           CheckIsBinarySearchTree<ValueT, CompT>(*root.right_child)));
+}
+
+template <typename ValueT, typename CompT>
+bool CheckIsBinarySearchTree(RBTreeNode<ValueT>* root,
+                             RBTreeNode<ValueT>* left_bound,
+                             RBTreeNode<ValueT>* right_bound,
+                             const CompT& cmp) {
+  return (is_null(left_bound) || RunCmp(left_bound, root, cmp)) &&
+         (is_null(right_bound) || RunCmp(root, right_bound, cmp)) &&
+         (!root->HasLeftChild() ||
+          CheckIsBinarySearchTree(root->left_child, left_bound, root, cmp)) &&
          (!root->HasRightChild() ||
-          (RunCmp(root, root->right_child, CompT()) &&
-           CheckIsBinarySearchTree<ValueT, CompT>(root->right_child)));
+          CheckIsBinarySearchTree(root->right_child, root, right_bound, cmp));
+}
+
+template <typename ValueT, typename CompT>
+bool CheckIsBinarySearchTree(const ValueT& root, const CompT& cmp) {
+  return (!root.HasLeftChild() ||
+          (RunCmp(*root.left_child, root, cmp) &&
+           CheckIsBinarySearchTree(*root.left_child, cmp))) &&  // left child
+         (!root.HasRightChild() ||
+          (RunCmp(root, *root.right_child, cmp) &&
+           CheckIsBinarySearchTree(*root.right_child, cmp)));
+}
+
+template <typename ValueT>
+int BlackToLeaves(RBTreeNode<ValueT>* x) {
+  if (is_null(x)) {
+    return 0;
+  }
+  const int left = BlackToLeaves(x->left_child);
+  const int right = BlackToLeaves(x->right_child);
+  if (left == right) {
+    return left + (x->IsColorBlack() ? 1 : 0);
+  }
+  return -1;
+}
+
+template <typename ValueT>
+bool CheckBlackEquals(RBTreeNode<ValueT>* x) {
+  const int left = BlackToLeaves(x->left_child);
+  const int right = BlackToLeaves(x->right_child);
+  return left == right;
+}
+
+template <typename ValueT>
+void PrintTree(RBTreeNode<ValueT>* x) {
+  if (is_null(x)) {
+    std::cout << "()";
+    return;
+  }
+  auto m = (x->IsColorBlack() ? "b" : "");
+  std::cout << "(" << m << x->value_;
+  if (x->HasLeftChild() || x->HasRightChild()) {
+    PrintTree(x->left_child);
+    std::cout << ",";
+    PrintTree(x->right_child);
+  }
+  std::cout << ")";
 }
 
 template <typename ValueT>
@@ -171,6 +221,13 @@ RBTreeNode<ValueT>* UpperBound(const ValueT& val, RBTreeNode<ValueT>* x,
   return y;
 }
 
+template <typename ValueT>
+inline bool is_null(const ValueT& ptr) {
+  return ptr == nullptr;
+}
+
+}  // namespace
+
 template <typename ValueT, typename CompT>
 class RBTree {
  public:
@@ -178,7 +235,7 @@ class RBTree {
 
   RBTree() : root_(nullptr), value_cmp_() {}
 
-  ~RBTree() {}
+  ~RBTree() { RemoveTree(root_); }
 
   // STL like iterators
   class iterator {
@@ -186,12 +243,12 @@ class RBTree {
     iterator() : node_(nullptr) {}
     iterator(const iterator& iter) : node_(iter.node_) {}
     iterator(RBTreeNodeT* node) : node_(node) {}
-    // prefix
+    // PreInc
     iterator& operator++() {
       node_ = trilib::TreeSuccessor(node_);
       return *this;
     }
-    // postfix
+    // PostInc
     iterator operator++(int) {
       iterator tmp(*this);
       node_ = trilib::TreeSuccessor(node_);
@@ -203,6 +260,7 @@ class RBTree {
 
    private:
     RBTreeNodeT* node_;
+    friend class RBTree;
   };
 
   iterator begin() { return iterator(TreeMinimum(root_)); }
@@ -262,11 +320,190 @@ class RBTree {
     return iterator(trilib::UpperBound(val, root_, value_cmp_));
   }
 
+  iterator Search(const ValueT& value) {
+    RBTreeNodeT* ptr = root_;
+    while (ptr != nullptr && value != ptr->value_) {
+      // DCHECK(ptr != nullptr);
+      if (value_cmp_(value, ptr->value_)) {
+        ptr = ptr->left_child;
+      } else {
+        ptr = ptr->right_child;
+      }
+    }
+    return iterator(ptr);
+  }
+
   bool IsBinarySearchTree() const {
-    return CheckIsBinarySearchTree<ValueT, CompT>(root_);
+    //    return is_null(root_) || CheckIsBinarySearchTree(*root_,
+    //    value_cmp_);
+    return is_null(root_) || CheckIsBinarySearchTree<ValueT, CompT>(
+                                 root_, nullptr, nullptr, value_cmp_);
+  }
+
+  bool IsBlackProperty() const {
+    return is_null(root_) || CheckBlackEquals(root_);
+  }
+
+  void Print() {
+    PrintTree(root_);
+    std::cout << std::endl;
+  }
+
+  void Delete(iterator node) {
+    RBTreeNodeT* z = node.node_;
+    RBTreeNodeT* y = z;
+    bool y_orig_is_black = y->IsColorBlack();
+    RBTreeNodeT* x = nullptr;
+    bool was_x_right = false;
+    if (!z->HasLeftChild()) {
+      x = z->parent;
+      was_x_right = z->SafeIsRightChild();
+      Transplant(z, z->right_child);
+    } else if (!z->HasRightChild()) {
+      x = z->parent;
+      was_x_right = z->SafeIsRightChild();
+      Transplant(z, z->left_child);
+    } else {                            // z has both parents
+      y = TreeMinimum(z->right_child);  // minimum never has left_child
+      y_orig_is_black = y->IsColorBlack();
+      if (y != z->right_child) {
+        x = y->parent;
+        Transplant(y, y->right_child);
+        y->right_child = z->right_child;
+        y->right_child->parent = y;
+      } else {
+        x = y;  // can x be null? yes.
+        was_x_right = true;
+      }
+      Transplant(z, y);
+      y->left_child = z->left_child;  // we know z has left child, and y doesn't
+      y->left_child->parent = y;
+      y->SetColor(z->IsColorBlack());
+    }
+    delete z;
+    if (y_orig_is_black && x != nullptr) {
+      DeleteFixup(x, was_x_right);
+    }
   }
 
  private:
+  void RemoveTree(RBTreeNodeT* x) {
+    if (x != nullptr) {
+      RemoveTree(x->left_child);
+      RemoveTree(x->right_child);
+      delete x;
+    }
+  }
+
+  RBTreeNodeT* RightChildDeleteFixup(RBTreeNodeT* x, bool* was_right) {
+    // black sibling and has red child
+    RBTreeNodeT* sibling = x->left_child;
+    if (is_null(sibling)) {
+      return nullptr;  // ?
+    }
+    if (sibling->IsColorRed()) {
+      RightRotate(x);
+      x->SetColorRed();
+      sibling->SetColorBlack();
+      sibling = x->left_child;
+    }
+    if (sibling->HasLeftChild() && sibling->left_child->IsColorRed()) {
+      sibling->left_child->SetColorBlack();
+      sibling->SetColor(x->IsColorBlack());
+      RightRotate(x);
+      return nullptr;
+    } else if (sibling->HasRightChild() && sibling->right_child->IsColorRed()) {
+      sibling->SetColor(x->IsColorBlack());
+      sibling->right_child->SetColor(x->IsColorBlack());
+      x->SetColorBlack();
+      LeftRotate(sibling);
+      RightRotate(x);
+      return nullptr;
+    }
+    // we can assume that sibling is black
+    if (x->IsColorRed() && sibling->IsColorBlack()) {
+      x->SetColorBlack();
+      sibling->SetColorRed();
+      return nullptr;
+    } else if (sibling->IsColorBlack()) {
+      sibling->SetColorRed();
+      if (x->HasParent()) {
+        *was_right = x->IsRightChild();
+      }
+      x = x->parent;
+      return x;
+    }
+    return nullptr;
+  }
+
+  RBTreeNodeT* LeftChildDeleteFixup(RBTreeNodeT* x, bool* was_right) {
+    // black sibling and has red child
+    RBTreeNodeT* sibling = x->right_child;
+    if (is_null(sibling)) {
+      return nullptr;  // ?
+    }
+    if (sibling->IsColorRed()) {
+      LeftRotate(x);
+      x->SetColorRed();
+      sibling->SetColorBlack();
+      sibling = x->right_child;
+    }
+    if (sibling->HasRightChild() && sibling->right_child->IsColorRed()) {
+      sibling->right_child->SetColorBlack();
+      sibling->SetColor(x->IsColorBlack());
+      x->SetColorBlack();
+      LeftRotate(x);
+      return nullptr;
+    } else if (sibling->HasLeftChild() && sibling->left_child->IsColorRed()) {
+      sibling->SetColor(x->IsColorBlack());
+      sibling->left_child->SetColor(x->IsColorBlack());
+      x->SetColorBlack();
+      RightRotate(sibling);
+      LeftRotate(x);
+      return nullptr;
+    }
+    // we can assume that sibling is black
+    if (x->IsColorRed() && sibling->IsColorBlack()) {
+      x->SetColorBlack();
+      sibling->SetColorRed();
+      return nullptr;
+    } else if (sibling->IsColorBlack()) {
+      sibling->SetColorRed();
+      if (x->HasParent()) {
+        *was_right = x->IsRightChild();
+      }
+      x = x->parent;
+      return x;
+    }
+    return nullptr;
+  }
+
+  void DeleteFixup(RBTreeNodeT* x, bool was_right_child_removed) {
+    bool was_right_child = was_right_child_removed;
+    while (x != nullptr) {
+      if (!was_right_child) {
+        x = LeftChildDeleteFixup(x, &was_right_child);
+      } else {
+        x = RightChildDeleteFixup(x, &was_right_child);
+      }
+    }
+  }
+
+  // Replaces subtree rooted at u with subtree rooted at v.
+  // v can be nullptr.
+  void Transplant(RBTreeNodeT* u, RBTreeNodeT* v) {
+    if (!u->HasParent()) {
+      root_ = v;
+    } else if (u->IsLeftChild()) {
+      u->parent->left_child = v;
+    } else {
+      u->parent->right_child = v;
+    }
+    if (v != nullptr) {
+      v->parent = u->parent;
+    }
+  }
+
   RBTreeNodeT* TreeMinimum(RBTreeNodeT* x) { return trilib::TreeMinimum(x); }
 
   RBTreeNodeT* TreeMaximum(RBTreeNodeT* x) { return trilib::TreeMaximum(x); }
@@ -285,7 +522,7 @@ class RBTree {
 
   RBTreeNodeT* Uncle(RBTreeNodeT* node) {
     RBTreeNodeT* g = GrandParent(node);
-    if (g == nullptr) {
+    if (is_null(g)) {
       return nullptr;                          // No grandparent means no uncle
     } else if (node->parent->IsLeftChild()) {  // node->properties & kLeftChild
       return g->right_child;
@@ -314,24 +551,24 @@ class RBTree {
 
   void RightRotate(RBTreeNodeT* x) {
     RBTreeNodeT* y = x->left_child;
-    x->left_child = y->right_child;
+    x->left_child = y->right_child;  // 1
     if (y->HasRightChild()) {
-      y->right_child->parent = x;
+      y->right_child->parent = x;  // 2
     }
-    y->parent = x->parent;
-    if (!x->HasParent()) {
+    y->parent = x->parent;  // 3
+    if (!x->HasParent()) {  // 4
       root_ = y;
     } else if (x->IsRightChild()) {
       x->parent->right_child = y;
     } else {
       x->parent->left_child = y;
     }
-    y->right_child = x;
-    x->parent = y;
+    y->right_child = x;  // 5
+    x->parent = y;       // 6
   }
 
   RBTreeNodeT* BinarySearchInsert(ValueT value) {
-    if (root_ == nullptr) {
+    if (is_null(root_)) {
       root_ = new RBTreeNodeT(value);
       return root_;
     }
--- src/rbtree_test.cc
+++ src/rbtree_test.cc
@@ -4,6 +4,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include <algorithm>
 #include <iostream>
 #include <functional>
 
@@ -17,6 +18,7 @@ TEST(RBTreeInt, CheckBinaryTreeTrivial) {
   EXPECT_TRUE(rbtree.IsBinarySearchTree()) << "after inserting 13";
   rbtree.Insert(11);
   EXPECT_TRUE(rbtree.IsBinarySearchTree()) << "after inserting 11";
+  ASSERT_TRUE(rbtree.IsBlackProperty());
 }
 
 TEST(RBTreeInt, CheckInsertIncreasing) {
@@ -26,6 +28,7 @@ TEST(RBTreeInt, CheckInsertIncreasing) {
     rbtree.Insert(i);
     ASSERT_TRUE(rbtree.IsBinarySearchTree()) << "after inserting " << i;
   }
+  ASSERT_TRUE(rbtree.IsBlackProperty());
 }
 
 TEST(RBTreeInt, CheckInsertDecreasing) {
@@ -35,6 +38,16 @@ TEST(RBTreeInt, CheckInsertDecreasing) {
     rbtree.Insert(i);
     ASSERT_TRUE(rbtree.IsBinarySearchTree()) << "after inserting " << i;
   }
+  ASSERT_TRUE(rbtree.IsBlackProperty());
+}
+
+TEST(RBTreeInt, CheckBigInsert) {
+  trilib::RBTree<int, less<int>> rbtree;
+  constexpr int insert_size = 1024000;
+  for (int i = insert_size; i != 0; --i) {
+    rbtree.Insert(i);
+  }
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
 }
 
 TEST(RBTreeInt, CheckInsertPerm) {
@@ -48,16 +61,39 @@ TEST(RBTreeInt, CheckInsertPerm) {
     if (val >= my_prime) {
       val -= my_prime;
     }
-    rbtree.Insert(i);
+    rbtree.Insert(val);
     ASSERT_TRUE(rbtree.IsBinarySearchTree()) << "after inserting " << i;
   }
 }
 
-TEST(RBTreeInt, CheckBigInsert) {
+TEST(RBTreeInt, CheckInsertDeletePerm) {
   trilib::RBTree<int, less<int>> rbtree;
-  constexpr int insert_size = 1024000;
+  constexpr int insert_size = 15;
+  constexpr int my_prime = 31;
+  constexpr int iter_val = 7;
+
+  int val = 0;
   for (int i = insert_size; i != 0; --i) {
-    rbtree.Insert(i);
+    val += iter_val;
+    if (val >= my_prime) {
+      val -= my_prime;
+    }
+    ASSERT_EQ(rbtree.end(), rbtree.Search(val));
+    rbtree.Insert(val);
+  }
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+
+  val = 0;
+  for (int i = insert_size; i != 0; --i) {
+    val += iter_val;
+    if (val >= my_prime) {
+      val -= my_prime;
+    }
+    auto iter = rbtree.Search(val);
+    ASSERT_EQ(val, *iter);
+    ASSERT_NE(iter, rbtree.end());
+    rbtree.Delete(iter);
+    ASSERT_TRUE(rbtree.IsBlackProperty()) << "after deleting " << val;
   }
   ASSERT_TRUE(rbtree.IsBinarySearchTree());
 }
@@ -73,9 +109,74 @@ TEST(RBTreeInt, CheckBigInsertPerm) {
     if (val >= my_prime) {
       val -= my_prime;
     }
-    rbtree.Insert(i);
+    rbtree.Insert(val);
   }
   ASSERT_TRUE(rbtree.IsBinarySearchTree());
+}
+
+// TEST(RBTreeInt, CheckVeryBigInsertRemovePerm) {
+//  trilib::RBTree<int, less<int>> rbtree;
+//  constexpr int my_prime = 30402457;
+//  constexpr int iter_val = 5678910;
+//  constexpr int insert_size = 3000000;
+//
+//  int val = 0;
+//  for (int i = insert_size; i != 0; --i) {
+//    val += iter_val;
+//    if (val >= my_prime) {
+//      val -= my_prime;
+//    }
+//    rbtree.Insert(val);
+//  }
+//  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+//
+//  val = 0;
+//  for (int i = insert_size; i != 0; --i) {
+//    val += iter_val;
+//    if (val >= my_prime) {
+//      val -= my_prime;
+//    }
+//    auto iter = rbtree.Search(val);
+//    ASSERT_EQ(val, *iter);
+//    ASSERT_NE(iter, rbtree.end());
+//    rbtree.Delete(iter);
+//  }
+//  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+//}
+
+int factorial(int x, int result = 1) {
+  if (x == 1)
+    return result;
+  else
+    return factorial(x - 1, x * result);
+}
+
+TEST(RBTreeInt, AllPermInsertDelete) {
+  trilib::RBTree<int, less<int>> rbtree;
+  constexpr int size = 4;
+  int values[size];
+  int del_values[size];
+  for (int i = 0; i < size; ++i) {
+    values[i] = i;
+    del_values[i] = i;
+  };
+
+  int perm_size = factorial(size);
+  for (int perm_ins = 0; perm_ins < perm_size; ++perm_ins) {
+    for (int perm = 0; perm < perm_size; ++perm) {
+      for (int i = 0; i < size; ++i) {
+        rbtree.Insert(values[i]);
+      }
+      ASSERT_TRUE(rbtree.IsBinarySearchTree());
+      for (int i = 0; i < size; ++i) {
+        rbtree.Delete(rbtree.Search(del_values[i]));
+        ASSERT_TRUE(rbtree.IsBlackProperty());
+      }
+      ASSERT_TRUE(rbtree.IsBinarySearchTree());
+      std::next_permutation(del_values, del_values + size);
+    }
+    std::next_permutation(values, values + size);
+  }
 }
 
 class IteratorFixture : public ::testing::Test {
@@ -148,3 +249,100 @@ TEST_F(IteratorFixture, UpperBound) {
   //  ASSERT_EQ(rbtree.end(), rbtree.UpperBound(15));
   ASSERT_EQ(rbtree.end(), rbtree.UpperBound(0));
 }
+
+TEST(RBTreeInt, DeleteOnlyOne) {
+  trilib::RBTree<int, less<int>> rbtree;
+  rbtree.Insert(12);
+  auto iter = rbtree.begin();
+  rbtree.Delete(iter);
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+  ASSERT_TRUE(rbtree.IsBlackProperty());
+}
+
+TEST(RBTreeInt, Destructor) {
+  trilib::RBTree<int, less<int>> rbtree;
+  rbtree.Insert(12);
+  rbtree.Insert(13);
+  rbtree.Insert(11);
+  auto iter = rbtree.begin();
+}
+
+TEST(RBTreeInt, DeleteLeaves) {
+  trilib::RBTree<int, less<int>> rbtree;
+  rbtree.Insert(12);
+  rbtree.Insert(11);
+  auto iter = rbtree.begin();
+  rbtree.Delete(iter);
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+  ASSERT_TRUE(rbtree.IsBlackProperty());
+
+  rbtree.Insert(13);
+  iter = rbtree.begin();
+  ++iter;
+  rbtree.Delete(iter);
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+  ASSERT_TRUE(rbtree.IsBlackProperty());
+}
+
+TEST(RBTreeInt, DeleteRoot) {
+  trilib::RBTree<int, less<int>> rbtree;
+  rbtree.Insert(12);
+  rbtree.Insert(11);
+  rbtree.Insert(13);
+  auto iter = rbtree.begin();
+  ++iter;  // 12
+  EXPECT_EQ(12, *iter);
+  rbtree.Delete(iter);
+  ASSERT_TRUE(rbtree.IsBinarySearchTree());
+  ASSERT_TRUE(rbtree.IsBlackProperty());
+}
+
+class FullTreeFixture : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    for (int i = 0; i < elems_size; ++i) {
+      rbtree.Insert(elems[i]);
+    }
+  }
+  static const int elems[];
+  static const int elems_size;
+  trilib::RBTree<int, less<int>> rbtree;
+};
+const int FullTreeFixture::elems[] = {8, 4, 12, 2, 6,  10, 14, 1,
+                                      3, 5, 7,  9, 11, 13, 15};
+const int FullTreeFixture::elems_size = 15;
+
+TEST_F(FullTreeFixture, Search) {
+  for (int i = 1; i < 16; ++i) {
+    ASSERT_EQ(i, *rbtree.Search(i));
+  }
+  ASSERT_EQ(rbtree.end(), rbtree.Search(0));
+  ASSERT_EQ(rbtree.end(), rbtree.Search(16));
+}
+
+#define TEST_FULL_TREE_DELETE(num)            \
+  TEST_F(FullTreeFixture, Delete##num) {      \
+    auto iter = rbtree.begin();               \
+    for (int i = 0; i < num; ++i) {           \
+      ++iter;                                 \
+    }                                         \
+    rbtree.Delete(iter);                      \
+    ASSERT_TRUE(rbtree.IsBinarySearchTree()); \
+    ASSERT_TRUE(rbtree.IsBlackProperty());    \
+  }
+
+TEST_FULL_TREE_DELETE(0);
+TEST_FULL_TREE_DELETE(1);
+TEST_FULL_TREE_DELETE(2);
+TEST_FULL_TREE_DELETE(3);
+TEST_FULL_TREE_DELETE(4);
+TEST_FULL_TREE_DELETE(5);
+TEST_FULL_TREE_DELETE(6);
+TEST_FULL_TREE_DELETE(7);
+TEST_FULL_TREE_DELETE(8);
+TEST_FULL_TREE_DELETE(9);
+TEST_FULL_TREE_DELETE(10);
+TEST_FULL_TREE_DELETE(11);
+TEST_FULL_TREE_DELETE(12);
+TEST_FULL_TREE_DELETE(13);
+TEST_FULL_TREE_DELETE(14);
